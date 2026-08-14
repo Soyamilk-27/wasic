ac0f66fa7e2a47bb412e257208788f10ee68d88b
Soyamilk-27 <cnd3d1a@gmail.com>
2026-08-14 22:23:28 +0800

    y = x llvm impl

--- wasic/codegen/llvm_codegen/node.h
+++ wasic/codegen/llvm_codegen/node.h
@@ -4,9 +4,23 @@
 
 inline llvm::Value* LLVMCodegen::generateNode(const Node& node) {
     switch (node.kind) {
-        case Node::Kind::Number: {
-            auto* num = builder.getInt32(std::stoll(node.text));
-            return num;
+        case Node::Kind::Number:
+            return builder.getInt32(std::stoll(node.text));
+
+        case Node::Kind::Name: {
+            auto it = variables.find(node.text);
+
+            if (it == variables.end()) {
+                throw std::runtime_error(
+                    "Unknown variable: " + node.text
+                );
+            }
+
+            return builder.CreateLoad(
+                builder.getInt32Ty(),
+                it->second,
+                node.text
+            );
         }
 
         default:
